d9c3538c311a3bcfa4e41c462e245b9e2ad168ae
Mitchell Goff <mitchellgoffpc@gmail.com>
2025-04-16 11:13:58 -0700

    build mupen64plus-core with DEBUGGER=1

--- lakitu/env/lib/build.sh
+++ lakitu/env/lib/build.sh
@@ -5,13 +5,18 @@ cd $(dirname "$0")
 
 echo "Building mupen64plus-core"
 cd mupen64plus-core/projects/unix
-make all
+sed -i '' 's/LDLIBS += -lopcodes -lbfd/# LDLIBS += -lopcodes -lbfd/' Makefile  # libopcodes and libbfd aren't needed for what we're doing
+make clean
+make all DEBUGGER=1
 mv libmupen64plus.* ../../../
+cp ../../data/font.ttf ../../../
+sed -i '' 's/# LDLIBS += -lopcodes -lbfd/LDLIBS += -lopcodes -lbfd/' Makefile  # change it back
 make clean
 cd ../../../
 
 echo "Building mupen64plus-audio-sdl"
 cd mupen64plus-audio-sdl/projects/unix
+make clean
 make all
 mv mupen64plus-audio-sdl.* ../../../
 make clean
@@ -19,6 +24,7 @@ cd ../../../
 
 echo "Building mupen64plus-rsp-hle"
 cd mupen64plus-rsp-hle/projects/unix
+make clean
 make all
 mv mupen64plus-rsp-hle.* ../../../
 make clean
@@ -26,6 +32,7 @@ cd ../../../
 
 echo "Building mupen64plus-input-ext"
 cd mupen64plus-input-ext/projects/unix
+make clean
 make all
 mv mupen64plus-input-ext.* ../../../
 make clean
